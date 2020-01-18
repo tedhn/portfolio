2ee369dbba0c16a7daeb8bb83dd900e2b81693cd
Ted <heinhtetnaing186@gmail.com>
2020-01-18 22:31:07 +0630

    Updates

---
+++ precache-manifest.a9272e138c0ff295fe3b136e77df0342.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69fee5ba6435a873c90cb4467559b37",
+    "url": "/Portforlio/index.html"
+  },
+  {
+    "revision": "3b593bc73811a25eddda",
+    "url": "/Portforlio/static/css/main.6b6cd8bf.chunk.css"
+  },
+  {
+    "revision": "46a70cf4ad61e916674b",
+    "url": "/Portforlio/static/js/2.14de64ee.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Portforlio/static/js/2.14de64ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b593bc73811a25eddda",
+    "url": "/Portforlio/static/js/main.9c40f9dc.chunk.js"
+  },
+  {
+    "revision": "d62b12f7b54fc3975e12",
+    "url": "/Portforlio/static/js/runtime-main.4a474681.js"
+  },
+  {
+    "revision": "51af2bae6d84ae82e57b7c7d6eed9e4b",
+    "url": "/Portforlio/static/media/Me.51af2bae.svg"
+  },
+  {
+    "revision": "617e093c9955e06f3936bbbb97b0d95d",
+    "url": "/Portforlio/static/media/amino.617e093c.svg"
+  },
+  {
+    "revision": "0d56f88264be5996ff4f07feb54dc7a0",
+    "url": "/Portforlio/static/media/discord.0d56f882.svg"
+  },
+  {
+    "revision": "957a38067309b80c03db11f28db6e168",
+    "url": "/Portforlio/static/media/dp.957a3806.svg"
+  },
+  {
+    "revision": "e471d0e59851508a99d2241e1585a8d5",
+    "url": "/Portforlio/static/media/welcome.e471d0e5.svg"
+  }
+]);
